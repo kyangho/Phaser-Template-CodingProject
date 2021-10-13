54e92258cda5d50966dd8960d6aeb96d9418e590
beanbeebee <beanbeebee@gmail.com>
2021-10-13 13:54:26 +0700

    add sound for female and male character

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -12,6 +12,9 @@ export default class MainScene extends Phaser.Scene{
         this.load.image('Text_1', 'assets/Text_Image.png');
         this.load.image('Text_2', 'assets/Text_Image_1.png');
         this.load.image('Logo', 'assets/pixel logo.png')
+        this.load.audio('Bg_sound', 'assets/background_sound.mp3');
+        this.load.audio('Male_sound', 'assets/male_sound.mp3');
+        this.load.audio('Female_sound', 'assets/female_sound.mp3');
     }
     //Nơi để thêm ra các đối tượng như image, text,... cần có trong Scene này
     create(){
@@ -21,15 +24,28 @@ export default class MainScene extends Phaser.Scene{
         var text_1 = this.add.image(500, 310, 'Text_1').setScale(0.7, 0.7).setVisible(false);
         var text_2 = this.add.image(175, 300, 'Text_2').setScale(0.7, 0.7).setVisible(false);
         var logo = this.add.image(350, 120, 'Logo').setScale(0.2, 0.2).setVisible(false);
-
+        var background_sound = this.sound.add('Bg_sound',{
+            volume: 0.3,
+            loop: true
+        }).play();
+        var male_sound = this.sound.add('Male_sound', {volume: 0.2});
+        var female_sound = this.sound.add('Female_sound', {volume: 0.2});
         female.setInteractive();
         female.on('pointerdown', () => {
-            text_1.setVisible(true);
+            if(text_2.visible && !male_sound.isPlaying){
+                if(!female_sound.isPlaying){
+                    female_sound.play();
+                }
+                text_1.setVisible(true);
+            }
         })
 
         male.setInteractive();
         male.on('pointerdown', () => {
             text_2.setVisible(true);
+            if(!male_sound.isPlaying){
+                male_sound.play();
+            }
         })
         
         background.setInteractive();
